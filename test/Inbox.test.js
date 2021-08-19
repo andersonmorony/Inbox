d1de28e07c3878eb9572c91999ebb08697de9f7a
andersonmorony <andersonmoroni92@gmail.com>
2021-08-19 19:44:05 -0300

    Deployment the contract

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,14 +2,23 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
 
-beforeEach(() => {
-    web3.eth.getAccounts()
-        .then(fAccounts => {
-        console.log(fAccounts);
-    });
+let accounts;
+let inbox;
+
+beforeEach( async () => {
+    //Get a list of all accounts
+    accounts = await web3.eth.getAccounts();
+
+    //Deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there'] })
+        .send({ from: accounts[0], gas: '1000000' });
 });
 
 describe('Inbox', () => {
-    it('Deploy one contact', () => {});
+    it('Deploy one contact', () => {
+        console.log(inbox);
+    });
 });
