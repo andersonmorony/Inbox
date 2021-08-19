d224bfe4828a3a4e80bf31a22f98fef11c2189a8
andersonmorony <andersonmoroni92@gmail.com>
2021-08-19 19:14:17 -0300

    Add mocha test

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,4 +1,15 @@
 const assert = require('assert');
-const ganache = require('ganache');
+const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = Web3(ganache.provider());
+const web3 = new Web3(ganache.provider());
+
+beforeEach(() => {
+    web3.eth.getAccounts()
+        .then(fAccounts => {
+        console.log(fAccounts);
+    });
+});
+
+describe('Inbox', () => {
+    it('Deploy one contact', () => {});
+});
